80040ce416ce9819bec889bcdfc9746ef28fd12a
Prakhar Beohar <prakharbeohar81@gmail.com>
2024-03-29 02:42:04 +0530

    About section complete

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,4 +1,5 @@
 import styles from "./App.module.css";
+import { About } from "./components/About/About";
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from './components/Navbar/Navbar';
 
@@ -8,7 +9,8 @@ function App() {
   return (
     <div className = {styles.App}>
       <Navbar />
-      <Hero />  
+      <Hero /> 
+      <About /> 
     </div>
   )
 }
