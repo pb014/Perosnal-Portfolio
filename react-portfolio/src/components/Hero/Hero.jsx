2305cf7e7c4b928459454e625db884a64c98eacd
Prakhar Beohar <prakharbeohar81@gmail.com>
2024-03-29 02:11:27 +0530

    Hero section complete

---
+++ react-portfolio/src/components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { getImageUrl } from '../../utils';
+import styles from "./Hero.module.css";
+
+export const Hero = () => {
+  return <section className={styles.container}>
+    <div className={styles.content}>
+        <h1 className={styles.title}>Hi I'm Prakhar Beohar</h1>
+        <p className={styles.description}>Front-End Developer and UI/UX Designer. Reach out if you'd like to learn more!</p>
+        <a href="mailto:prakharbeohar81@gmail.com" className={styles.contactBtn}>Contact Me</a>
+    </div>
+    <img src={getImageUrl("hero/heroImage.png")} alt ="Hero image of me" className={styles.heroImg}/>
+    <div className={styles.topBlur} />
+    <div className={styles.bottomBlur}/>
+  </section>
+}
